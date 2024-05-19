a58d112a46e14189a0fb1af42200f9445bc2ee37
himan07 <himan9714@gmail.com>
2024-05-19 17:54:19 +0530

    changes

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -8,10 +8,10 @@ const HomeLayout = ({ children }: { children: ReactNode }) => {
       <Navbar />
       <div className="flex">
         <Sidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md: pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
       </div>
-      <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md: pb-14 sm:px-14">
-        <div className="w-full">{children}</div>
-      </section>
     </main>
   );
 };
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 import React from "react";
 import { SidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-
+import Image from "next/image";
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -12,16 +12,28 @@ const Sidebar = () => {
     <section className="sticky left-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {SidebarLinks.map((links) => {
-          const isActive = pathname === links.route || pathname.startsWith(links.route);
+          const isActive =
+            pathname === links.route || pathname.startsWith(links.route);
           return (
-            <Link 
-              href={links.route} 
-              key={links.lable} 
-              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
-                'bg-blue-1': isActive
-              })}
+            <Link
+              href={links.route}
+              key={links.lable}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
             >
-              {links.lable}
+              <Image
+                src={links.imgUrl}
+                alt={links.lable}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {links.lable}
+              </p>
             </Link>
           );
         })}
