47ead089025e5dc50901737e9913d73731127414
Himanshu162 <himanshu@costacloud.com>
2024-05-19 19:45:45 +0530

    update

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Previous = () => {
+  return (
+    <div>
+      Previous
+    </div>
+  )
+}
+
+export default Previous
---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Upcoming = () => {
+  return (
+    <div>
+      Upcoming
+    </div>
+  )
+}
+
+export default Upcoming
