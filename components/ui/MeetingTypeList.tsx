2894ef372d5b420332bdf16cde2acccb379eea58
Himanshu162 <himanshu@costacloud.com>
2024-05-25 10:27:14 +0530

    changes

---
+++ components/ui/MeetingTypeList.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react"; 
+import Image from "next/image";
+
+
+const MeetingTypeList = () => {
+  return (
+    <section className="grid gird-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <div
+        className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
+        onClick={(e) => console.log(e)}
+      >
+        <div className="flex-center glassmorphism size-12 rounded-[10px]">
+        <Image src="/icons/add-meeting.svg" alt="meeting" width={27} height={27}  />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold ">New Meeting</h1>
+          <p className="text-lg font-normal">Start an instant meeting</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MeetingTypeList;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,42 +23,18 @@ const config = {
           1: "#1C1F2E",
           2: "#161925",
         },
-        blue:{
-          1:"#0E78F9"
+        blue: {
+          1: "#0E78F9",
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        sky: {
+          1: "#C9DDFF",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+        orange: {
+          1: "#FF742E",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+      },
+      backgroundImage: {
+        hero: "url('/images/hero-background.png')",
       },
       borderRadius: {
         lg: "var(--radius)",
