013491c243a13b49bcfc706aaf227a55094b68d0
Himanshu162 <himanshu@costacloud.com>
2024-05-27 15:33:17 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <ClerkProvider
         appearance={{
           layout: {
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "",
             socialButtonsVariant: "iconButton",
           },
           variables: {
